c56fcdf428cedb5c847398c240e1970020d63ca1
umurcan emre <ibrahimumurcanemre@hotmail.com>
2020-01-30 21:30:30 +0300

    port property added, unit test ensuring service starts added

--- src/test/java/com/umurcanemre/mancala/MancalaApplicationTests.java
+++ src/test/java/com/umurcanemre/mancala/MancalaApplicationTests.java
@@ -1,5 +1,6 @@
 package com.umurcanemre.mancala;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class MancalaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void mainRuns() {
+		new MancalaApplication().main(Arrays.array(""));
 	}
 
 }
